0593447a1a5e95c46b79f692994f078c1b3b7dd4
MarkSauter <dr.funk.mark@gmail.com>
2016-12-29 15:19:34 -0600

    initial commit

---
+++ home.elm
@@ -0,0 +1,15 @@
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Markdown
+import String
+
+import Center
+import Skeleton
+
+
+
+main =
+  Skeleton.skeleton "home"
+    []
+
---
+++ src/shared/Skeleton.elm
@@ -0,0 +1,57 @@
+module Skeleton exposing (skeleton)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+(=>) : a -> b -> (a, b)
+(=>) = (,)
+
+
+skeleton : String -> List (Html msg) -> Html msg
+skeleton tabName content =
+  div []
+    (header tabName :: content ++ [footer])
+
+
+
+-- HEADER
+
+
+header : String -> Html msg
+header name =
+  div [ id "tabs" ]
+    [ a 
+      [ href "/"
+      , style
+        [ "position" => "absolute"
+        , "left" => "1em"
+        , "top" => "1em"
+        ]
+      ]
+      [ img [ src "/assets/logo.svg", style [ "width" => "24px" ] ] []
+      ]
+    , ul []  (List.map (tab name) [ "examples", "docs", "community", "blog" ])
+    ]
+
+
+tab : String -> String -> Html msg
+tab currentName name =
+  li []
+    [ a 
+      [ classList [ "tab" => True, "current" => (currentName == name) ]
+      , href ("/" ++ name)
+      ]
+      [ text name ]
+    ]
+
+
+-- FOOTER
+
+footer : Html msg
+footer =
+  div [ class "footer"]
+    [ text "All code for this site is open source and written in Elm. "
+    , a [ class "grey-link", href "https://github.com/elm-lang/elm-lang.org/" ] [ text "Check it out" ]
+    , text "! - © 2012-2016 Evan Czaplicki"
+    ]
